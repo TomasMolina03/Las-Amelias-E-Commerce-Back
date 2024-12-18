800ad3b1b07571fafacc6c0534ad81f3f7805c7b
MaximoFC <maximofcallejas@gmail.com>
2024-12-18 16:30:56 -0300

    Helmet added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
